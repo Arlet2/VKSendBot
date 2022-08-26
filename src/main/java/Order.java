8b5e901022af895e8f82c40c985cfae613d7f3a1
korlir <anonym.show@mail.ru>
2022-08-26 10:19:42 +0300

    Add order for sending

---
+++ src/main/java/Order.java
@@ -0,0 +1,17 @@
+public class Order {
+    private final String msg;
+    private final String[] names;
+
+    Order(String msg, String[] names) {
+        this.msg = msg;
+        this.names = names;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public String[] getNames() {
+        return names;
+    }
+}
