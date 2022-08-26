724f2f7396a1f32e5ea392a5dd3fa08b22b7e441
korlir <anonym.show@mail.ru>
2022-08-26 10:00:31 +0300

    Add service for creating reports after executing some orders

---
+++ src/main/java/ReportService.java
@@ -0,0 +1,29 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalTime;
+
+public class ReportService {
+    private final String FILE_NAME = "report";
+
+    private StringBuffer stringBuffer;
+
+    public void createNewReport() {
+        stringBuffer = new StringBuffer();
+    }
+
+    public void addMessageToReport(String msg) {
+        stringBuffer.append(msg).append("\n");
+    }
+
+    public void finalReport() {
+        try {
+            FileWriter writer = new FileWriter(FILE_NAME+"-"+LocalTime.now().toString());
+
+            writer.write(stringBuffer.toString());
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("ERROR WITH REPORT SAVING");
+        }
+    }
+}
