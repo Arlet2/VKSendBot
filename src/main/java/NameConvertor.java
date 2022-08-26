1e907ad2b1c48f93e7f99b92c73c9311dee770eb
korlir <anonym.show@mail.ru>
2022-08-26 10:33:17 +0300

    Add convertor

---
+++ src/main/java/NameConvertor.java
@@ -0,0 +1,20 @@
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.GroupActor;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.objects.utils.responses.ResolveScreenNameResponse;
+
+public class NameConvertor {
+    private final VkApiClient api;
+    private final GroupActor groupActor;
+
+    NameConvertor(VkApiClient api, GroupActor groupActor) {
+        this.api = api;
+        this.groupActor = groupActor;
+    }
+
+    public int convertNameToId(String name) throws ClientException, ApiException {
+        ResolveScreenNameResponse response = api.utils().resolveScreenName(groupActor, name).execute();
+        return response.getObjectId();
+    }
+}
