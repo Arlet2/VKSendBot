defdeeb96e53065033b05d9403cec84118cece49
korlir <anonym.show@mail.ru>
2022-08-26 11:23:18 +0300

    Add file viewer

---
+++ src/main/java/utils/FileViewer.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class FileViewer {
+
+    public static List<String> getAllFilesFromDir(String dirName) {
+        File dir = new File(dirName);
+
+        if (Optional.ofNullable(dir.listFiles()).isEmpty())
+            return null;
+
+        return Arrays.stream(dir.listFiles())
+                .map(File::getName)
+                .filter(name -> name.matches(".*\\.order"))
+                .collect(Collectors.toList());
+    }
+}
