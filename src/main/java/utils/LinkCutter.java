f0a5e80665c92f326cb1d2d008c69e9cb52e896c
Arlet <shulga@niuitmo.ru>
2022-09-29 17:47:20 +0300

    Add LinkCutter prototype

---
+++ src/main/java/utils/LinkCutter.java
@@ -0,0 +1,37 @@
+package utils;
+
+public class LinkCutter {
+    private LinkCutter() {
+
+    }
+
+    public static String cutLinks(String link) {
+        link = cutMobileFullLink(link);
+        link = cutMobileShortLink(link);
+        link = cutFullLink(link);
+        link = cutShortLink(link);
+        link = clearSpaces(link);
+        return link;
+    }
+
+    private static String clearSpaces(String link) {
+        return link.replace(" ", "");
+    }
+
+    private static String cutMobileFullLink(String link) {
+        return link.replace("https://m.vk.com/", "");
+    }
+
+    private static String cutMobileShortLink(String link) {
+        return link.replace("m.vk.com/", "");
+    }
+
+    private static String cutFullLink(String link) {
+        return link.replace("https://vk.com/", "");
+    }
+
+    private static String cutShortLink(String link) {
+        return link.replace("vk.com/", "");
+    }
+
+}
