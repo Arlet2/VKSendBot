a88dbc5475ac9e83f93cbb4e4a9a5a5b66070fdf
korlir <anonym.show@mail.ru>
2022-10-08 22:29:23 +0300

    Add realization of console UI in application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,17 +4,13 @@ import com.vk.api.sdk.client.actors.GroupActor;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
 import auth.AuthData;
 import auth.AuthDataReader;
-import order.Order;
-import order.OrderParser;
-import utils.FileViewer;
-import utils.NotFoundByRegexException;
+import services.NameConvertorService;
+import services.SendService;
+import ui.ConsoleUI;
+import ui.UI;
 
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
 
 public class Application {
     private static AuthData authData;
@@ -24,8 +20,6 @@ public class Application {
 
     private static SendService sendService;
 
-    private static boolean isProgramInterrupted;
-
     public static void main(String[] args) {
 
         readAuthInfo();
@@ -34,7 +28,9 @@ public class Application {
 
         initServices();
 
-        start();
+        UI ui = new ConsoleUI(sendService);
+
+        ui.startInteraction();
     }
 
     private static void readAuthInfo() {
@@ -59,103 +55,4 @@ public class Application {
         sendService = new SendService(api, groupActor, nameConvertorService);
     }
 
-    private static void start() {
-        String[] input;
-        Scanner scanner = new Scanner(System.in);
-
-        isProgramInterrupted = false;
-
-        System.out.println("Welcome to SendBot v. " +
-                Order.class.getPackage().getImplementationVersion() + " (by Arlet)!");
-        System.out.println("https://github.com/Arlet2/VKSendBot for guide for this application");
-        System.out.println("\nUse help to see all commands");
-
-        while (!isProgramInterrupted) {
-            System.out.print("$ ");
-
-            input = scanner.nextLine().split(" ");
-
-            switch (input[0]) {
-                case "q":
-                case "quit":
-                case "exit":
-                    exitCommand();
-                    break;
-                case "help":
-                    helpCommand();
-                    break;
-                case "view":
-                    viewCommand(input);
-                    break;
-                case "run":
-                case "r":
-                case "exe":
-                case "execute":
-                    executeCommand(scanner, input);
-                    break;
-                default:
-                    System.out.println("Unknown command. Please, use help to see all commands");
-                    break;
-            }
-        }
-    }
-
-    private static void exitCommand() {
-        isProgramInterrupted = true;
-        System.out.println("Exiting...");
-    }
-
-    private static void helpCommand() {
-        System.out.println("help - view this text");
-        System.out.println("exit - exit from application (also exit, quit, q)");
-        System.out.println("execute fileName - execute .order file (also run, r, exe)");
-        System.out.println("view dirName - view all .order files on directory");
-    }
-
-    private static void viewCommand(String[] input) {
-        List<String> fileNames;
-        if (input.length == 1)
-            fileNames = FileViewer.getAllFilesFromDir("");
-        else
-            fileNames = FileViewer.getAllFilesFromDir(input[1]);
-
-        if (input.length == 1)
-            System.out.println("Files .order on this directory...");
-        else
-            System.out.println("Files .order on " + input[1] + "...");
-
-        if (Optional.ofNullable(fileNames).isPresent())
-            fileNames.forEach(System.out::println);
-        else
-            System.out.println("No any files.");
-
-    }
-
-    private static void executeCommand(Scanner scanner, String[] input) {
-        Order order;
-        try {
-            order = OrderParser.parseOrder(input[1]);
-        } catch (IOException e) {
-            System.out.println("File " + input[1] + " is not found");
-            e.printStackTrace();
-            return;
-        } catch (NotFoundByRegexException e) {
-            System.out.println("The file is incorrect. Please, read documentation and see examples.");
-            return;
-        }
-        System.out.println("ORDER MESSAGE:");
-        System.out.println(order.getMsg());
-
-        System.out.println("Are you sure to continue? (y/n)");
-        input[0] = scanner.nextLine();
-
-        if (input[0].equals("y") || input[0].equals("yes")) {
-            long startTime = System.currentTimeMillis();
-            sendService.executeSendOrder(order);
-            long endTime = System.currentTimeMillis();
-            System.out.println("Executed for "+ (endTime - startTime) + " ms");
-        }
-        else
-            System.out.println("Order is declined");
-    }
 }
