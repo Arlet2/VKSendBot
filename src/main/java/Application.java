d42f861afd8d175ba1c594a2b253a51a33047f86
korlir <anonym.show@mail.ru>
2022-10-08 19:53:09 +0300

    Split commands to functions

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -24,6 +24,8 @@ public class Application {
 
     private static SendService sendService;
 
+    private static boolean isProgramInterrupted;
+
     public static void main(String[] args) {
 
         readAuthInfo();
@@ -61,14 +63,14 @@ public class Application {
         String[] input;
         Scanner scanner = new Scanner(System.in);
 
-        boolean isInterrupted = false;
+        isProgramInterrupted = false;
 
         System.out.println("Welcome to SendBot v. " +
                 Order.class.getPackage().getImplementationVersion() + " (by Arlet)!");
         System.out.println("https://github.com/Arlet2/VKSendBot for guide for this application");
         System.out.println("\nUse help to see all commands");
 
-        while (!isInterrupted) {
+        while (!isProgramInterrupted) {
             System.out.print("$ ");
 
             input = scanner.nextLine().split(" ");
@@ -77,68 +79,83 @@ public class Application {
                 case "q":
                 case "quit":
                 case "exit":
-                    isInterrupted = true;
-                    System.out.println("Exiting...");
+                    exitCommand();
                     break;
                 case "help":
-                    System.out.println("help - view this text");
-                    System.out.println("exit - exit from application (also exit, quit, q)");
-                    System.out.println("execute fileName - execute .order file (also run, r, exe)");
-                    System.out.println("view dirName - view all .order files on directory");
+                    helpCommand();
                     break;
                 case "view":
-                    List<String> fileNames;
-                    if (input.length == 1)
-                        fileNames = FileViewer.getAllFilesFromDir("");
-                    else
-                        fileNames = FileViewer.getAllFilesFromDir(input[1]);
-
-                    if (input.length == 1)
-                        System.out.println("Files .order on this directory...");
-                    else
-                        System.out.println("Files .order on " + input[1] + "...");
-
-                    if (Optional.ofNullable(fileNames).isPresent())
-                        fileNames.forEach(System.out::println);
-                    else
-                        System.out.println("No any files.");
-
+                    viewCommand(input);
                     break;
                 case "run":
                 case "r":
                 case "exe":
                 case "execute":
-                    Order order;
-                    try {
-                        order = OrderParser.parseOrder(input[1]);
-                    } catch (IOException e) {
-                        System.out.println("File " + input[1] + " is not found");
-                        e.printStackTrace();
-                        break;
-                    } catch (NotFoundByRegexException e) {
-                        System.out.println("The file is incorrect. Please, read documentation and see examples.");
-                        break;
-                    }
-                    System.out.println("ORDER MESSAGE:");
-                    System.out.println(order.getMsg());
-
-                    System.out.println("Are you sure to continue? (y/n)");
-                    input[0] = scanner.nextLine();
-
-                    if (input[0].equals("y") || input[0].equals("yes")) {
-                        long startTime = System.currentTimeMillis();
-                        sendService.executeSendOrder(order);
-                        long endTime = System.currentTimeMillis();
-                        System.out.println("Executed for "+ (endTime - startTime) + " ms");
-                    }
-                    else
-                        System.out.println("Order is declined");
-
+                    executeCommand(scanner, input);
                     break;
                 default:
                     System.out.println("Unknown command. Please, use help to see all commands");
                     break;
             }
         }
+    }
+
+    private static void exitCommand() {
+        isProgramInterrupted = true;
+        System.out.println("Exiting...");
+    }
+
+    private static void helpCommand() {
+        System.out.println("help - view this text");
+        System.out.println("exit - exit from application (also exit, quit, q)");
+        System.out.println("execute fileName - execute .order file (also run, r, exe)");
+        System.out.println("view dirName - view all .order files on directory");
+    }
+
+    private static void viewCommand(String[] input) {
+        List<String> fileNames;
+        if (input.length == 1)
+            fileNames = FileViewer.getAllFilesFromDir("");
+        else
+            fileNames = FileViewer.getAllFilesFromDir(input[1]);
+
+        if (input.length == 1)
+            System.out.println("Files .order on this directory...");
+        else
+            System.out.println("Files .order on " + input[1] + "...");
+
+        if (Optional.ofNullable(fileNames).isPresent())
+            fileNames.forEach(System.out::println);
+        else
+            System.out.println("No any files.");
+
+    }
+
+    private static void executeCommand(Scanner scanner, String[] input) {
+        Order order;
+        try {
+            order = OrderParser.parseOrder(input[1]);
+        } catch (IOException e) {
+            System.out.println("File " + input[1] + " is not found");
+            e.printStackTrace();
+            return;
+        } catch (NotFoundByRegexException e) {
+            System.out.println("The file is incorrect. Please, read documentation and see examples.");
+            return;
+        }
+        System.out.println("ORDER MESSAGE:");
+        System.out.println(order.getMsg());
+
+        System.out.println("Are you sure to continue? (y/n)");
+        input[0] = scanner.nextLine();
+
+        if (input[0].equals("y") || input[0].equals("yes")) {
+            long startTime = System.currentTimeMillis();
+            sendService.executeSendOrder(order);
+            long endTime = System.currentTimeMillis();
+            System.out.println("Executed for "+ (endTime - startTime) + " ms");
+        }
+        else
+            System.out.println("Order is declined");
     }
 }
