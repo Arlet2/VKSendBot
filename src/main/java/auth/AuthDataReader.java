f75a8c8ad0f6fe293c1a7b8348c57424f30fdd04
korlir <anonym.show@mail.ru>
2022-10-10 03:52:40 +0300

    Fix file reading

--- src/main/java/auth/AuthDataReader.java
+++ src/main/java/auth/AuthDataReader.java
@@ -5,6 +5,7 @@ import com.google.gson.GsonBuilder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class AuthDataReader {
     private AuthDataReader() {
@@ -12,12 +13,20 @@ public class AuthDataReader {
     }
 
     public static AuthData readAuthJson(String path) throws FileNotFoundException {
-        FileReader reader = new FileReader(path);
-
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
 
-        return gson.fromJson(reader, AuthData.class);
+        FileReader reader = new FileReader(path);
+
+        AuthData authData = gson.fromJson(reader, AuthData.class);
+
+        try {
+            reader.close();
+        } catch (IOException ignored) {
+            // программа способна работать и без закрытого файла
+        }
+
+        return authData;
     }
 
     public static AuthData readEncryptedFile(String path) throws FileNotFoundException {
