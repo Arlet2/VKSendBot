08b8f147ad722bf51953a8b531d448d37de6bb7d
korlir <anonym.show@mail.ru>
2022-10-10 03:33:21 +0300

    Add changing of group actor

--- src/main/java/services/NameConvertorService.java
+++ src/main/java/services/NameConvertorService.java
@@ -8,15 +8,18 @@ import com.vk.api.sdk.objects.utils.responses.ResolveScreenNameResponse;
 
 public class NameConvertorService {
     private final VkApiClient api;
-    private final GroupActor groupActor;
+    private GroupActor groupActor;
 
-    public NameConvertorService(VkApiClient api, GroupActor groupActor) {
+    public NameConvertorService(VkApiClient api) {
         this.api = api;
-        this.groupActor = groupActor;
     }
 
     public int convertNameToId(String name) throws ClientException, ApiException {
         ResolveScreenNameResponse response = api.utils().resolveScreenName(groupActor, name).execute();
         return response.getObjectId();
+    }
+
+    public void changeGroupActor(GroupActor groupActor) {
+        this.groupActor = groupActor;
     }
 }
