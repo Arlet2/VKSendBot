d28d6e5c1bcb7e13fdc23a2da6e126b6ed207902
korlir <anonym.show@mail.ru>
2022-10-11 03:10:19 +0300

    Add json reader

---
+++ src/main/java/utils/file_readers/AuthJsonReader.java
@@ -0,0 +1,28 @@
+package utils.file_readers;
+
+import auth.AuthData;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class AuthJsonReader implements FileReader<AuthData> {
+    @Override
+    public AuthData read(String path) throws FileNotFoundException {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        java.io.FileReader reader = new java.io.FileReader(path);
+
+        AuthData authData = gson.fromJson(reader, AuthData.class);
+
+        try {
+            reader.close();
+        } catch (IOException ignored) {
+            // программа способна работать и без закрытого файла
+        }
+
+        return authData;
+    }
+}
