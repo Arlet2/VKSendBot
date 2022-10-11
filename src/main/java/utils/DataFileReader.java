357ca5e246e97d8a06d61ae11d46cf72a7692d19
korlir <anonym.show@mail.ru>
2022-10-11 03:11:50 +0300

    Fix file reader

--- src/main/java/utils/DataFileReader.java
+++ src/main/java/utils/DataFileReader.java
@@ -1,12 +1,9 @@
 package utils;
 
-import auth.AuthData;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import utils.exceptions.FileReadingException;
+import utils.file_readers.FileReader;
 
 import java.io.*;
-import java.io.FileReader;
 import java.util.Optional;
 
 public class DataFileReader {
@@ -14,40 +11,10 @@ public class DataFileReader {
 
     }
 
-    public static AuthData readAuthJson(String path) throws FileNotFoundException {
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-
-        FileReader reader = new FileReader(path);
-
-        AuthData authData = gson.fromJson(reader, AuthData.class);
-
-        try {
-            reader.close();
-        } catch (IOException ignored) {
-            // программа способна работать и без закрытого файла
-        }
-
-        return authData;
-    }
-
-    public static Object readEncryptedFile(String path) throws FileNotFoundException {
-        try {
-            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(path));
-            Object object = objectInputStream.readObject();
-
-            return object;
-        } catch (IOException e) {
-            throw new FileReadingException("Cannot open "+path);
-        } catch (ClassNotFoundException e) {
-            throw new FileReadingException("Incorrect data was saved");
-        }
-    }
-
-    public static <T> Optional<T> readFile(utils.FileReader<T> reader) {
+    public static <T> Optional<T> readFile(FileReader<T> reader, String path) {
         T data;
         try {
-            data = reader.read();
+            data = reader.read(path);
         } catch (FileNotFoundException e) {
             System.out.println("File is not found");
             return Optional.empty();
