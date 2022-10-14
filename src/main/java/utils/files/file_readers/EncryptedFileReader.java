512ee00bcbc5952acff5220d9f3bfbc783c72311
korlir <anonym.show@mail.ru>
2022-10-14 22:33:14 +0300

    Add reader of encrypted files

---
+++ src/main/java/utils/files/file_readers/EncryptedFileReader.java
@@ -0,0 +1,31 @@
+package utils.files.file_readers;
+
+import utils.Serializer;
+import utils.encryptors.EncryptorService;
+import utils.encryptors.EncryptorsFactory;
+import utils.encryptors.SimpleEncryptor;
+import utils.exceptions.FileReadingException;
+import utils.files.ByteObject;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class EncryptedFileReader implements FileReader<Object> {
+    private final EncryptorService encryptor = EncryptorsFactory.getEncryptor(SimpleEncryptor.ENCRYPTION_PROTOCOL);
+
+    @Override
+    public Object read(String path) throws FileNotFoundException {
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(path));
+            ByteObject byteObject = (ByteObject) objectInputStream.readObject();
+
+            return Serializer.convertBytesToObject(encryptor.decrypt(byteObject.getBytes()));
+        } catch (IOException e) {
+            throw new FileReadingException("Cannot open " + path);
+        } catch (ClassNotFoundException e) {
+            throw new FileReadingException("Incorrect data was saved");
+        }
+    }
+}
