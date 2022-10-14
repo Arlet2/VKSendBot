36c2af7b2fbbda3e48e60bbcba0068c45953a277
korlir <anonym.show@mail.ru>
2022-10-14 22:33:31 +0300

    Add class-wraper of data for saving on files

---
+++ src/main/java/utils/files/ByteObject.java
@@ -0,0 +1,13 @@
+package utils.files;
+
+public class ByteObject {
+    private final byte[] bytes;
+
+    public ByteObject(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    public byte[] getBytes() {
+        return bytes;
+    }
+}
