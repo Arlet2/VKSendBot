4e0214049e1e18697cf847845e102766feddc3e8
korlir <anonym.show@mail.ru>
2023-04-15 22:02:20 +0300

    Add icon

--- src/main/kotlin/ui/graphic/GraphicalUI.kt
+++ src/main/kotlin/ui/graphic/GraphicalUI.kt
@@ -4,11 +4,16 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.res.useResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import order.Order
 import services.SendService
 import ui.UI
+import java.awt.Image
 
 class GraphicalUI(private val sendService: SendService) : UI(sendService) {
     @Composable
@@ -24,30 +29,51 @@ class GraphicalUI(private val sendService: SendService) : UI(sendService) {
                 if (sendService.groupActor != null) sendService.groupActor.accessToken ?: "" else ""
             )
         }
+
+        val errorDialogMessage = remember { mutableStateOf("") }
+        val infoDialogMessage = remember { mutableStateOf("") }
+
         Row(modifier = Modifier.fillMaxSize()) {
             leftPanel(
                 modifier = Modifier.weight(1f),
                 token = token,
                 groupId = groupId,
-                sendService = sendService
+                sendService = sendService,
+                errorDialogMessage = errorDialogMessage,
+                infoDialogMessage = infoDialogMessage
             )
             centerPanel(
                 modifier = Modifier.weight(1f),
                 order = order,
                 groupId = groupId,
-                token = token
+                token = token,
+                sendService = sendService,
+                errorDialogMessage = errorDialogMessage,
+                infoDialogMessage = infoDialogMessage
             )
             rightPanel(
                 modifier = Modifier.weight(1f),
-                order
+                order = order,
+                errorDialogMessage = errorDialogMessage,
+                infoDialogMessage = infoDialogMessage
             )
         }
+
+        if (errorDialogMessage.value != "")
+            ErrorDialog({ errorDialogMessage.value = "" }, message = errorDialogMessage.value)
+
+        if (infoDialogMessage.value != "")
+            InfoDialog({ infoDialogMessage.value = "" }, message = infoDialogMessage.value)
     }
 
 
     override fun startInteraction() {
         application {
-            Window(onCloseRequest = ::exitApplication, title = "VKSendBot") {
+            Window(
+                onCloseRequest = ::exitApplication,
+                title = "VKSendBot",
+                icon = BitmapPainter(useResource("icon.png", ::loadImageBitmap))
+            ) {
                 MaterialTheme {
                     application()
                 }
