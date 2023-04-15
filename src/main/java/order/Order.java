30ad9fda2c6766a5ee0483c29c559a6d8bedbc0f
korlir <anonym.show@mail.ru>
2023-04-15 03:02:29 +0300

    Make order public

--- src/main/java/order/Order.java
+++ src/main/java/order/Order.java
@@ -4,7 +4,7 @@ public class Order {
     private final String msg;
     private final String[] names;
 
-    Order(String msg, String[] names) {
+    public Order(String msg, String[] names) {
         this.msg = msg;
         this.names = names;
     }
