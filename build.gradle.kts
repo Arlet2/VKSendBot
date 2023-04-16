048ef7aac5a68592b4395e832078ce3b94c61124
korlir <anonym.show@mail.ru>
2023-04-16 03:11:21 +0300

    Change version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "space.arlet"
-version = "1.3.0-beta"
+version = "1.3.0-final"
 
 val artifactId = "VKSendBot"
 
